2626cffedbba8ee34e6b204aa528e0122b4be479
Ziga <shehu.meni@gmail.com>
2025-03-20 15:28:29 +0100

    Added scrolling with Page Up and Down, added moving to the end of the file with the End key

--- zedit.c
+++ zedit.c
@@ -415,6 +415,14 @@ void editorProcessKeyPress() {
 
   case PAGE_UP:
   case PAGE_DOWN: {
+    if (c == PAGE_UP) {
+      E.cy = E.rowoff;
+    } else if (c == PAGE_DOWN) {
+      E.cy = E.rowoff + E.screenrows - 1;
+      if (E.cy > E.numrows)
+        E.cy = E.numrows;
+    }
+
     int times = E.screenrows;
     while (times--)
       editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
@@ -423,7 +431,9 @@ void editorProcessKeyPress() {
     E.cx = 0;
     break;
   case END_KEY:
-    E.cx = E.screencols - 1;
+    if (E.cy < E.numrows)
+      E.cx = E.row[E.cy].size;
+    /*E.cx = E.screencols - 1;*/
     break;
   case ARROW_UP:
   case ARROW_DOWN:
